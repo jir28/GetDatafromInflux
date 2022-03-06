c1e69fa2d118cefcf8999e236a077d124bdd22c5
jir28 <2203068000@correo.ler.uam.mx>
2022-03-06 13:02:10 -0600

    Commit

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import random
 import influxdb_client
-from influxdb_client.client.write_api import SYNCHRONOUS
 import numpy as np
+
+
+def sumValues(array_Lits):
+    sumTim = round((np.sum(array_Lits) / 60), 2)
+    print(sumTim)
+    return sumTim
+
+
+def promT(arrTem):
+    return np.mean(arrTem)
 
 
 def get_data_querys(query_info):
@@ -16,22 +25,31 @@ def get_data_querys(query_info):
     )
     query_api = client.query_api()
     query_T = query_info
-
+    aux = 0
+    aux1 = 0
     result1 = query_api.query(org=org, query=query_T)
     arr_tiempo = []
+    aux_Litus = []
+    auxCel = []
     for table in result1:
         for record in table.records:
             arr_tiempo.append((record.get_value()))
+            if record.get_field() == 'Segundos' or record.get_field() == 'Litros' and aux == 0:
+                aux = 1
+                aux_Litus.append((record.get_field()))
+            elif record.get_field() == 'Celsius' and aux1 == 0:
+                aux1 = 1
+                auxCel.append((record.get_field()))
     ar_li = np.asarray(arr_tiempo).reshape(1, -1)
-    return ar_li
+    if aux_Litus:
+        valor = sumValues(ar_li)
+    elif auxCel:
+        valor = promT(ar_li)
+
+    return valor
 
 
-queryTime = ' from(bucket:"ShowerS")\
-        |> range(start: -30d)\
-        |> filter(fn:(r) => r._measurement == "Tiempo")\
-        |> filter(fn:(r) => r._field == "Segundos" ) '
-
-# Temperatura prom
+# Tiempos para 30 dias
 queryTemp = ' from(bucket:"ShowerS")\
        |> range(start: -30d)\
        |> filter(fn:(r) => r._measurement == "TemperaturaProm")\
@@ -46,31 +64,29 @@ queryTime = ' from(bucket:"ShowerS")\
         |> filter(fn:(r) => r._measurement == "Tiempo")\
         |> filter(fn:(r) => r._field == "Segundos" ) '
 
-
 #####################
-# Temperatura prom
-queryTemp15 = ' from(bucket:"ShowerS")\
-       |> range(start: -18h, stop: -15h)\
+# Tiempos para una semana
+queryTemp7 = ' from(bucket:"ShowerS")\
+       |> range(start: -7d, stop: now)\
        |> filter(fn:(r) => r._measurement == "TemperaturaProm")\
        |> filter(fn:(r) => r._field == "Celsius" ) '
 
-queryLiters15 = ' from(bucket:"ShowerS")\
-            |> range(start: -18h, stop: -10h)\
+queryLiters7 = ' from(bucket:"ShowerS")\
+            |> range(start: -7d, stop: now)\
             |> filter(fn:(r) => r._measurement == "Litros")\
             |> filter(fn:(r) => r._field == "Litros" ) '
 
-queryTime15 = ' from(bucket:"ShowerS")\
-        |> range(start: -18h, stop: -10h)\
+queryTime7 = ' from(bucket:"ShowerS")\
+        |> range(start: -7d, stop: now)\
         |> filter(fn:(r) => r._measurement == "Tiempo")\
         |> filter(fn:(r) => r._field == "Segundos" ) '
 
+Lits = get_data_querys(queryLiters)
+tiempos = get_data_querys(queryTime)
+tempes = get_data_querys(queryTemp)
+# promTim = round((np.sum(Tim))/60, 2)
+print("Litros de baño : ", Lits)
+print("Minutos de baño : ", tiempos)
+print("TempProm de baño : ", tempes)
 
-print("Tiempos")
-Tim = get_data_querys(queryTime15)
-print("Temperaturas")
-Celsius = get_data_querys(queryTemp15)
-print("Litros")
-Lits = get_data_querys(queryLiters15)
-promTim = round((np.sum(Tim))/60, 2)
-print("Minutos de baño : ", promTim)
 # obtain()
